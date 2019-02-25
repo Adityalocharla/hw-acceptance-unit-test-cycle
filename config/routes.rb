170212fcba93e94815b43664471c19b829f413cb
EC2 Default User <ec2-user@ip-172-31-34-248.us-east-2.compute.internal>
2019-02-25 23:46:34 +0000

    find movies with director path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+  get 'movies/:id/find_moviebydirector' => 'movies#find_moviebydirector', as: :find_moviebydirector
 end
